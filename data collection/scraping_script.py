61dab94e1d5ed76ad534177e895c5ecb1c928b1a
Pedro Ribeiro <pedro.rocha257@gmail.com>
2023-06-06 17:43:23 -0300

    Comentarios no codigo

--- data collection/scraping_script.py
+++ data collection/scraping_script.py
@@ -10,40 +10,54 @@ from google.oauth2.service_account import Credentials
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+# Definição das permissões necessárias para acesso às planilhas do Google
 scopes = ['https://www.googleapis.com/auth/spreadsheets',
           'https://www.googleapis.com/auth/drive']
 
+# Carregar as credenciais a partir do arquivo JSON 'gbq.json' e definir as permissões
 credentials = Credentials.from_service_account_file('gbq.json', scopes=scopes)
 
+# Autorizar o acesso às planilhas do Google usando as credenciais
 gc = gspread.authorize(credentials)
 
+# Autenticação no Google Drive
 gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
 
-# open a google sheet
+# Abrir uma planilha do Google
 gs = gc.open_by_key('1nVyKyf4dVwrCleXtGz3gH7b6Hvs08pLsDr_ENfwL4LM')
 
+# URL da página a ser acessada
 url = "https://fbref.com/en/comps/24/Serie-A-Stats"
 
+# Cabeçalhos HTTP para simular um navegador
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
 
 try:
+    # Abrir a URL e obter o conteúdo HTML da página
     response = urlopen(url)
     html = response.read()
     
 except HTTPError as e:
+    # Lidar com erros HTTP, caso ocorram
     print(e.status, e.reason)
     
 except URLError as e:
+    # Lidar com erros de URL, caso ocorram
     print(e.reason)
 
+# Função para tratar o HTML removendo espaços extras e quebras de linha
 def trata_html(input):
     html = input.decode('utf-8')
     return " ".join(html.split()).replace('> <','><')
 
+# Tratar o HTML obtido
 html = trata_html(html)
+
+# Criar um objeto BeautifulSoup a partir do HTML tratado
 soup = BeautifulSoup(html, 'html.parser')
 
+# Dicionário que mapeia o nome da página na planilha com o ID da tabela no HTML
 dict_tables = {
   "Classificação": "results2023241_overall",
   "Estats Time" :  "stats_squads_standard_for",
@@ -59,22 +73,32 @@ dict_tables = {
   "Diversos" : "stats_squads_misc_for"
 }
 
+# Iterar sobre as páginas e IDs das tabelas no dicionário
 for pagina, id in dict_tables.items():
+  # Encontrar a tabela no HTML usando o ID
   soup_tabela = soup.find('table', id = id).find_all('tbody')
   
   lista_equipes = []
+  # Iterar sobre as linhas da tabela
   for equipe in soup_tabela[0].find_all('tr'):
     dict_equipe = {}
+    # Obter o nome da equipe a partir da coluna 'th' e seu valor correspondente
     dict_equipe[equipe.find('th').get('data-stat')] = equipe.find('th').getText()
+    # Iterar sobre as colunas de informações da equipe
     for info in equipe.find_all('td'):
+        # Obter o nome da coluna a partir do atributo 'data-stat' e seu valor correspondente
         dict_equipe[info.get('data-stat')] = info.getText()
     
     lista_equipes.append(dict_equipe)
 
+  # Converter a lista de dicionários em um DataFrame usando pandas
   df = pd.DataFrame(lista_equipes)
 
+  # Obter a planilha correspondente à página
   pagina = gs.worksheet(pagina)
+  # Limpar o conteúdo da planilha
   pagina.clear()
+  # Preencher a planilha com os dados do DataFrame, incluindo o cabeçalho das colunas
   set_with_dataframe(
     worksheet=pagina, 
     dataframe=df,
@@ -82,4 +106,5 @@ for pagina, id in dict_tables.items():
     include_column_header=True,
     resize=True)
 
+  # Imprimir a mensagem de atualização da tabela
   print(f"Tabela {pagina} atualizada.")
